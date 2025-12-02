b87dbf58311f82e6212c9289602676c9a2e4103d
Guilherme Freitas <guilhermefsantos150@gmail.com>
2025-12-01 23:36:44 -0300

    Agno agent

---
+++ default_agent.py
@@ -0,0 +1,30 @@
+from agno.agent import Agent
+from agno.models.google import Gemini
+from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.tools.valyu import ValyuTools
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+agent = Agent(
+    id="Agente de pesquisa",
+    name="Pesquisador DDG",
+    role="Responda perguntas baseadas em contextos buscados na internet e em artigos científicos",
+    instructions=[
+        "Busque as principais fontes confiáveis sobre o tema.",
+        "Entregue uma saída concisa com: 5-10 fatos em bullets e um quadro de fontes com título + URL.",
+        "Não invente links. Priorize sites oficiais, artigos e referências robustas.",
+        "Use a ValyuTools para busca em artigos e DuckDuckGoTools para buscas na internet."
+    ],
+    model=Gemini(
+        id="gemini-2.5-flash-lite",
+    ),
+    tools=[
+        DuckDuckGoTools(),
+        ValyuTools(),
+    ]
+)
+
+print(agent.print_response("Quais os principais artigos sobre redes neurais?"))
